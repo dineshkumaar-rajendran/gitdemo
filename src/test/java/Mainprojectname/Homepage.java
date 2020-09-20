4462e1ebcae672b9d97b50fbe4fd94d0f1442c59
Dinesh <dineshkumaar.rajendrand@gmail.com>
2020-09-20 20:30:33 +0530

    First Commit

---
+++ src/test/java/Mainprojectname/Homepage.java
@@ -0,0 +1,22 @@
+package Mainprojectname;
+
+import org.testng.annotations.Test;
+
+public class Homepage {
+	
+	
+	
+	@Test
+	
+	public void method() {
+		
+		System.out.println("Print 1");
+	}
+
+public void method1() {
+		
+		System.out.println("Print 2");
+	}
+	
+	
+}
